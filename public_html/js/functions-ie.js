3d7e74bef0627e074003b807b97b3cba38165e12
simonroth <git@simonroth.ch>
2020-05-01 23:46:56 +0200

    GitHub Pages

--- public_html/js/functions-ie.js
+++
@@ -1,7 +0,0 @@
-// css3 selectors
-// --------------------------------------------------------------
-
-$( document ).ready( function() {
-	// last-child
-	$( 'li:last-child' ).addClass( 'last-child' );
-} );
